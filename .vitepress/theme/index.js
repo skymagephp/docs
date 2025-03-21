81eed8a17f9fb29cd17fb303342c3775073a9ed7
bangnokia <bangnokia@gmail.com>
2025-03-22 05:02:30 +0700

    wip

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,15 +1,6 @@
 import DefaultTheme from 'vitepress/theme'
 import './custom.css'
-import CodeGroup from './components/CodeGroup.vue'
-import ImageCompare from './components/ImageCompare.vue'
-import FeatureCard from './components/FeatureCard.vue'
 
 export default {
   extends: DefaultTheme,
-  enhanceApp({ app }) {
-    // Register custom components
-    app.component('CodeGroup', CodeGroup)
-    app.component('ImageCompare', ImageCompare)
-    app.component('FeatureCard', FeatureCard)
-  }
 }
